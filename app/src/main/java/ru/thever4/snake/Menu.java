a406e14db31fd6116b9a795120d6a99d6e2c677d
Nikita <Nikita@TheVer4.ru>
2020-11-16 01:44:43 +0500

    Updated "Learn more" activity

--- app/src/main/java/ru/thever4/snake/Menu.java
+++ app/src/main/java/ru/thever4/snake/Menu.java
@@ -2,13 +2,40 @@ package ru.thever4.snake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[] activityButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        activityButtons = new Button[] {findViewById(R.id.buttonToIITPage), findViewById(R.id.buttonToGitHub)};
+        for(Button b : activityButtons)
+            b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonToIITPage:
+                openLinkInBrowser("https://vk.com/ab_csu_iit");
+                break;
+            case R.id.buttonToGitHub:
+                openLinkInBrowser("https://github.com/TheVer4edu/DroidSnake");
+                break;
+        }
+    }
+
+    private void openLinkInBrowser(String link) {
+        Uri uri = Uri.parse(link);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
